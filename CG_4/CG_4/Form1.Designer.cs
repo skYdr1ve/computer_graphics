68d93b029ac84b763f78dc54f48c12a35f49f4cd
skYdr1ve <mdvoryakov@gmail.com>
2020-04-18 00:32:37 +0300

    add task4

---
+++ CG_4/CG_4/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace CG_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(647, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(716, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(647, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(63, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(716, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(63, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(713, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(644, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(713, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Y1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(647, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(63, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(717, 149);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(62, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(800, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(62, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(797, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Radius";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(644, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Algorithms";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(797, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Time(ns): 0.0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(337, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(19, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "15";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(618, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(19, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "30";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 330);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "15";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 612);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "30";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(18, 25);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(13, 13);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 638);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Rasterization Algorithms";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ CG_4/CG_4/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CG_4
+{
+    public partial class Form1 : Form
+    {
+
+        private Bitmap _sLessBitmap;
+        private Bitmap _sMoreBitmap;
+        private readonly Stopwatch _stopWatch = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _sLessBitmap = new Bitmap(30, 30);
+            _sMoreBitmap = new Bitmap(600, 600);
+            InitGrid();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            comboBox1.Items.Add("Step-by-step");
+            comboBox1.Items.Add("DDA");
+            comboBox1.Items.Add("Bresenham`s line");
+            comboBox1.Items.Add("Bresenham`s circle");
+        }
+
+        private void InitGrid()
+        {
+            for (var y = 0; y < _sMoreBitmap.Height; y++)
+            {
+                for (var x = 0; x < _sMoreBitmap.Width; x++)
+                {
+                    if (x % 20 == 0 || y % 20 == 0)
+                        _sMoreBitmap.SetPixel(x, y, Color.Black);
+                }
+            }
+
+            pictureBox1.Image = _sMoreBitmap;
+            pictureBox1.Invalidate();
+        }
+
+        private void ClearImage()
+        {
+            _sLessBitmap = new Bitmap(30, 30);
+            _sMoreBitmap = new Bitmap(600, 600);
+            InitGrid();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label7.Text = "Time(ns): 0.0";
+            ClearImage();
+            _stopWatch.Start();
+            if (comboBox1.SelectedItem == "Step-by-step") 
+            {
+                pictureBox1.Image = StepByStep();
+            }
+            else if (comboBox1.SelectedItem == "DDA")
+            {
+                pictureBox1.Image = Dda();
+            }
+            else if (comboBox1.SelectedItem == "Bresenham`s line")
+            {
+                pictureBox1.Image = DrawLineBres();
+            }
+            else if (comboBox1.SelectedItem == "Bresenham`s circle")
+            {
+                pictureBox1.Image = CircleBres();
+            }
+            _stopWatch.Stop();
+            label7.Text = $"Time(ns): {_stopWatch.Elapsed.Ticks}";
+            _stopWatch.Reset();
+        }
+
+        public Bitmap FillBitmap()
+        {
+            var diffHeight = _sMoreBitmap.Height / _sLessBitmap.Height;
+            var diffWidth = _sMoreBitmap.Width / _sLessBitmap.Width;
+            for (var y = 0; y < _sLessBitmap.Height; y++)
+            {
+                for (var x = 0; x < _sLessBitmap.Width; x++)
+                {
+                    if (Color.Black.ToArgb() != _sLessBitmap.GetPixel(x, y).ToArgb()) continue;
+
+                    for (var i = 0; i < diffHeight; i++)
+                    {
+                        for (var j = 0; j < diffWidth; j++)
+                        {
+                            _sMoreBitmap.SetPixel(x * diffHeight + i, y * diffWidth + j, Color.Black);
+                        }
+                    }
+                }
+            }
+
+            return _sMoreBitmap;
+        }
+
+        private Bitmap StepByStep()
+        {
+            var x0 = Convert.ToInt32(textBox1.Text);
+            var y0 = Convert.ToInt32(textBox2.Text);
+            var x1 = Convert.ToInt32(textBox3.Text);
+            var y1 = Convert.ToInt32(textBox4.Text);
+
+            CheckArguments(ref x0, ref y0, ref x1, ref y1);
+
+            int x, y;
+            double k;
+            if (Math.Abs(x1 - x0) >= Math.Abs(y1 - y0))
+            {
+                k = 1.0 * (y1 - y0) / (x1 - x0);
+                for (x = Math.Min(x0, x1); x <= Math.Max(x0, x1); ++x)
+                {
+                    y = (int)Math.Round(k * (x - x0) + y0);
+                    _sLessBitmap.SetPixel(x, y, Color.Black);
+                }
+            }
+            else
+            {
+                k = 1.0 * (x1 - x0) / (y1 - y0);
+                for (y = Math.Min(y0, y1); y <= Math.Max(y0, y1); ++y)
+                {
+                    x = (int)Math.Round(k * (y - y0) + x0);
+                    _sLessBitmap.SetPixel(x, y, Color.Black);
+                }
+            }
+
+            FillBitmap();
+
+            return _sMoreBitmap;        
+        }
+
+        private Bitmap Dda()
+        {
+            var x0 = Convert.ToInt32(textBox1.Text);
+            var y0 = Convert.ToInt32(textBox2.Text);
+            var x1 = Convert.ToInt32(textBox3.Text);
+            var y1 = Convert.ToInt32(textBox4.Text);
+
+            CheckArguments(ref x0, ref y0, ref x1, ref y1);
+
+            var dx = x1 - x0;
+            var dy = y1 - y0;
+
+            var steps = Math.Abs(dx) > Math.Abs(dy) ? Math.Abs(dx) : Math.Abs(dy);
+
+            var xinc = dx / (float)steps;
+            var yinc = dy / (float)steps;
+
+            var x = (float)x0;
+            var y = (float)y0;
+            for (var i = 0; i <= steps; i++)
+            {
+                _sLessBitmap.SetPixel(Convert.ToInt32(x), Convert.ToInt32(y), Color.Black);
+                x += xinc;
+                y += yinc;
+            }
+
+            FillBitmap();
+
+            return _sMoreBitmap;
+        }
+
+        private Bitmap DrawLineBres()
+        {
+            var x0 = Convert.ToInt32(textBox1.Text);
+            var y0 = Convert.ToInt32(textBox2.Text);
+            var x1 = Convert.ToInt32(textBox3.Text);
+            var y1 = Convert.ToInt32(textBox4.Text);
+
+            CheckArguments(ref x0, ref y0, ref x1, ref y1);
+
+            int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+            int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+            int err = (dx > dy ? dx : -dy) / 2, e2;
+            for (; ; )
+            {
+                _sLessBitmap.SetPixel(x0, y0, Color.Black);
+                if (x0 == x1 && y0 == y1) break;
+                e2 = err;
+                if (e2 > -dx) { err -= dy; x0 += sx; }
+
+                if (e2 >= dy) continue;
+                err += dx; y0 += sy;
+            }
+
+            FillBitmap();
+
+            return _sMoreBitmap;
+        }
+
+        private bool CheckBorder(int x, int y)
+        {
+            return x <= 29 && y <= 29 && x >= 0 && y >= 0;
+        }
+
+        private void DrawCircle(int xc, int yc, int x, int y)
+        {
+            if (CheckBorder(xc + x, yc + y))
+            {
+                _sLessBitmap.SetPixel(xc + x, yc + y, Color.Black);
+            }
+            if (CheckBorder(xc - x, yc + y))
+            {
+                _sLessBitmap.SetPixel(xc - x, yc + y, Color.Black);
+            }
+            if (CheckBorder(xc + x, yc - y))
+            {
+                _sLessBitmap.SetPixel(xc + x, yc - y, Color.Black);
+            }
+            if (CheckBorder(xc - x, yc - y))
+            {
+                _sLessBitmap.SetPixel(xc - x, yc - y, Color.Black);
+            }
+            if (CheckBorder(xc + y, yc + x))
+            {
+                _sLessBitmap.SetPixel(xc + y, yc + x, Color.Black);
+            }
+            if (CheckBorder(xc - y, yc + x))
+            {
+                _sLessBitmap.SetPixel(xc - y, yc + x, Color.Black);
+            }
+            if (CheckBorder(xc + y, yc - x))
+            {
+                _sLessBitmap.SetPixel(xc + y, yc - x, Color.Black);
+            }
+            if (CheckBorder(xc - y, yc - x))
+            {
+                _sLessBitmap.SetPixel(xc - y, yc - x, Color.Black);
+            }
+        }
+
+        private Bitmap CircleBres()
+        {
+            var xc = Convert.ToInt32(textBox1.Text);
+            var yc = Convert.ToInt32(textBox2.Text);
+            var r = Convert.ToInt32(textBox6.Text);
+
+            CheckArguments(ref xc, ref yc, ref xc, ref yc);
+
+            int x = 0, y = r;
+            var d = 3 - 2 * r;
+
+            DrawCircle(xc, yc, x, y);
+
+            while (y >= x)
+            {
+                x++;
+
+                if (d > 0)
+                {
+                    y--;
+                    d = d + 4 * (x - y) + 10;
+                }
+                else
+                {
+                    d = d + 4 * x + 6;
+                }
+
+                DrawCircle(xc, yc, x, y);
+            }
+
+            FillBitmap();
+
+            return _sMoreBitmap;
+        }
+
+        private void CheckArguments(ref int x0, ref int y0, ref int x1, ref int y1)
+        {
+            if (x0 < 0)
+            {
+                x0 = 0;
+            }
+            else if (x0 > 29)
+            {
+                x0 = 29;
+            }
+
+            if (x1 < 0)
+            {
+                x1 = 0;
+            }
+            else if (x1 > 29)
+            {
+                x1 = 29;
+            }
+
+            if (y0 < 0)
+            {
+                y0 = 0;
+            }
+            else if (y1 > 29)
+            {
+                y1 = 29;
+            }
+
+            if (y1 < 0)
+            {
+                y1 = 0;
+            }
+            else if (y1 > 29)
+            {
+                y1 = 29;
+            }
+        }
+
+    }
+}
